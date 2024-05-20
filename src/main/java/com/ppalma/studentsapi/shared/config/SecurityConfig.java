6b3a8e66482d3f5f604f94a49c25f5f1399443b9
pedroapalmao <alexpedro6@gmail.com>
2024-05-20 17:54:40 -0300

    Add OAuth2 server with keycloak

---
+++ src/main/java/com/ppalma/studentsapi/shared/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.ppalma.studentsapi.shared.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((authz) -> authz
+            .requestMatchers(HttpMethod.GET, "/api/v1/students/**")
+            .hasAuthority("SCOPE_students/basic")
+            .requestMatchers(HttpMethod.POST, "/api/v1/students")
+            .hasAuthority("SCOPE_students/basic")
+            .requestMatchers(HttpMethod.DELETE, "/api/v1/students/**")
+            .hasAuthority("SCOPE_students/basic")
+            .requestMatchers(HttpMethod.GET, "/api/v1/students")
+            .hasAuthority("SCOPE_students/basic")
+            .requestMatchers(HttpMethod.POST, "/api/v1/students/average-notes")
+            .hasAuthority("SCOPE_students/average-notes")
+            .anyRequest().authenticated()
+        )
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+    return http.build();
+  }
+}
